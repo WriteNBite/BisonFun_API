3573a94f9b1d008993b5bb5317987269ec55e5f1
Alexander <writenbite@protonmail.com>
2024-08-08 18:08:12 +0300

    style(type-mapper): use ValueMapping annotation
    - use ValueMapping instead of default methods

--- src/main/java/com/writenbite/bisonfun/api/types/mapper/UserVideoContentListStatusMapper.java
+++ src/main/java/com/writenbite/bisonfun/api/types/mapper/UserVideoContentListStatusMapper.java
@@ -3,25 +3,15 @@ package com.writenbite.bisonfun.api.types.mapper;
 import com.writenbite.bisonfun.api.database.entity.UserVideoContentStatus;
 import com.writenbite.bisonfun.api.types.uservideocontent.UserVideoContentListStatus;
 import org.mapstruct.Mapper;
+import org.mapstruct.ValueMapping;
 
 @Mapper
 public interface UserVideoContentListStatusMapper {
-    default UserVideoContentStatus toUserVideoContentStatus(UserVideoContentListStatus listStatus){
-        return switch (listStatus){
-            case WATCHING -> UserVideoContentStatus.WATCHING;
-            case PLANNING -> UserVideoContentStatus.PLANNED;
-            case COMPLETED -> UserVideoContentStatus.COMPLETE;
-            case DROPPED -> UserVideoContentStatus.DROPPED;
-            case PAUSED -> UserVideoContentStatus.PAUSED;
-        };
-    }
-    default UserVideoContentListStatus toApiStatus(UserVideoContentStatus listStatus){
-        return switch (listStatus){
-            case WATCHING -> UserVideoContentListStatus.WATCHING;
-            case PLANNED -> UserVideoContentListStatus.PLANNING;
-            case COMPLETE -> UserVideoContentListStatus.COMPLETED;
-            case DROPPED -> UserVideoContentListStatus.DROPPED;
-            case PAUSED -> UserVideoContentListStatus.PAUSED;
-        };
-    }
+    @ValueMapping(target = "PLANNED", source = "PLANNING")
+    @ValueMapping(target = "COMPLETE", source = "COMPLETED")
+    UserVideoContentStatus toUserVideoContentStatus(UserVideoContentListStatus listStatus);
+
+    @ValueMapping(target = "PLANNING", source = "PLANNED")
+    @ValueMapping(target = "COMPLETED", source = "COMPLETE")
+    UserVideoContentListStatus toApiStatus(UserVideoContentStatus listStatus);
 }
